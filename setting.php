935c3c5d576715dc9ffbb5bf911a1dd73a391163
rutti <mail@rutti.net>
2014-10-07 06:00:25 +0900

    2nd
    setting.phpを修正

--- setting.php
+++ setting.php
@@ -8,11 +8,11 @@ define("META_DESC","オープンソースの簡易掲示板システムRami-BBSです。");
 // BBSの特徴となるキーワードをカンマ区切りで入力
 define("META_KEYWORD","BBS,Rami-BBS,掲示板,オープンソース");
 
+// // ベースとなるURL(最後はスラッシュで閉じる)
+define("BASE_URL","./");
+
 // 管理人の名前
 define("AUTHOR","rutti.net");
-
-// ベースとなるURL(最後はスラッシュで閉じる)
-define("BASE_URL","http://test.rutti.net/BBS/");
 
 // スキン選択
 define("SKIN_TYPE","default");
